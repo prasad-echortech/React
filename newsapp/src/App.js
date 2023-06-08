d47af71b6c289092e3f6874795a30ea0e2943a61
prasad <prasad.s@echortech.com>
2023-06-08 10:03:41 +0530

    08/06/2023 NewsMonkey app

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <div>
-          <h1>hello </h1>
+        <NavBar />
+        <div className="container">
+          <News />
         </div>
+
       </div>
     );
   }
